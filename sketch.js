430c4c2c3e02855c6fb156a8a6ab7148f5b06724
jan2017-lab <68855710+jan2017-lab@users.noreply.github.com>
2020-07-30 18:54:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var frect,mrect;
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  frect = createSprite(200,200,50,80);
+  frect.shapeColor = "green";
+  mrect = createSprite(400,200,80,30);
+  mrect.shapeColor = "green";
+
+}
+
+function draw() {
+  background("black"); 
+  
+  mrect.x = World.mouseX;
+  mrect.y = World.mouseY;
+
+  if(mrect.x - frect.x < frect.width/2 + mrect.width/2 && frect.x - mrect.x < frect.width/2 + mrect.width/2
+    && mrect.y - frect.y < frect.height/2 + mrect.height/2 && frect.y - mrect.y < frect.height/2 + mrect.height/2) {
+  mrect.shapeColor = "red";
+  frect.shapeColor = "red";
+}
+else{
+mrect.shapeColor = "green";
+frect.shapeColor = "green";
+
+
+
+}
+
+  drawSprites();
+}
